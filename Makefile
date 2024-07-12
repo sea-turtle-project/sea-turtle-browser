58da3f1611e99818df25b1a67ca752cf93b8e9d1
Tathya Garg <tathya.garg@gmail.com>
2024-07-12 17:12:17 +0530

    Added SDL basic window

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -I/usr/include/python3.12/ src/main.cpp -L/usr/lib/python3.12/config-3.12-x86_64-linux-gnu -lpython3.12 -o bin/main.o
+	g++ -I/usr/include/python3.12/ -I/usr/include/SDL2/ src/main.cpp -L/usr/lib/python3.12/config-3.12-x86_64-linux-gnu -lpython3.12 -lSDL2 -o bin/main.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+
+class Window {
+    SDL_Window* window;
+
+    public:
+
+    Window(const char* title) : window{SDL_CreateWindow(title, 0, 0, 800, 600, SDL_WINDOW_SHOWN)} {}
+    ~Window() { SDL_DestroyWindow(window); }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        std::cerr << "Failed to initalize SDL: " << SDL_GetError();
+    else {
+        Window window("Seaturtle Browser");
+        SDL_Delay(5000);
+    }
+
+    SDL_Quit();
+
+    return 0;
 }
